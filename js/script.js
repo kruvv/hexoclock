7cf1ba5e915dc95b21dfd715d45f8a87a5b758f3
kruvv <kruvv@mail.ru>
2021-07-05 17:40:24 +0300

    Добавил адаптивную верстку, исправил макет главной страницы

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-var clock = document.getElementById('clock');
-var color = document.getElementById('color');
+const clock = document.getElementById('clock');
+const color = document.getElementById('color');
 
 
 function hexoClock() {
-  var time = new Date();
-  var h = (time.getHours() % 12).toString();
-  var m = time.getMinutes().toString();
-  var s = time.getSeconds().toString();
+  const time = new Date();
+  let h = (time.getHours() % 12).toString();
+  let m = time.getMinutes().toString();
+  let s = time.getSeconds().toString();
 
 //check if the number of digits is less than two then we add zero from the front
+
   if(h.length < 2) {
     h = '0' + h;
   }
@@ -20,8 +21,8 @@ function hexoClock() {
   }
 
 //write data to variables
-  var clockString = h + ':' + m + ':' + s;
-  var colorString = '#' + h + m + s;
+  const clockString = h + ':' + m + ':' + s;
+  const colorString = '#' + h + m + s;
 
   clock.textContent = clockString;
   color.textContent = colorString;
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 #hexoClock
 
 This clock changes the background color depending on the time.
-Эти часы меняют цвет фона в зависимости от времени.
+Часы меняют цвет фона в зависимости от времени.
